3387a58ceef13c6afec9f9b1ae8fe3062ff8e77d
drupchen <hhdrupchen@gmail.com>
2019-04-29 22:31:54 +0200

    update

--- prepare_error_dataset.py
+++ prepare_error_dataset.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import time
 import datetime
-
+import re
 
 from bo_freq_diff import SegmentDiff, SentenceOrderedDiff
-from pybo import BoTokenizer, Token
+from pybo import BoTokenizer, Token, PyBoChunk
 from textunits import sentencify
 
 rpl_start, rpl_middle, rpl_end = '{~~', '~>', '~~}'
@@ -12,6 +12,28 @@ del_start, del_end = '{--', '--}'
 pls_start, pls_end = '{++', '++}'
 marks = [rpl_start, rpl_middle, rpl_end, del_start, del_end, pls_start, pls_end]
 tok = BoTokenizer('GMD', ignore_chars=['\n'])
+
+
+class BoNonboChunk(PyBoChunk):
+    def __init__(self, string):
+        super().__init__(string, ignore_chars=['\n'])
+
+    def chunk(self, indices=True, gen=False):
+        return self.chunk_bo_chars()
+
+    def get_cleaned_bo(self):
+        chunks = self.chunk()
+        chunks = self.get_chunked(chunks)
+        has_bo = True if self.BO_MARKER in [a[0] for a in chunks] else False
+        chunks = [b for a, b in chunks if a == self.BO_MARKER]
+        return ''.join(chunks).strip(), has_bo
+
+
+def clean_non_bo(string):
+    chunk = BoNonboChunk(string)
+    cleaned, _ = chunk.get_cleaned_bo()
+    cleaned = cleaned.strip()
+    return cleaned
 
 
 def gen_sent_pair(sent):
@@ -23,6 +45,13 @@ def gen_sent_pair(sent):
             word = word.strip()
         else:
             space = ''
+
+        end = ''
+
+        word = word.replace(rpl_start + rpl_middle + rpl_end, '')
+        word = word.replace(del_start + del_end, '')
+        word = word.replace(pls_start + pls_end, '')
+
         if word.startswith(rpl_start) and word.endswith(rpl_end) and rpl_middle in word:
             to_del, to_add = word.replace(rpl_start, '').replace(rpl_end, '').split(rpl_middle)
             orig.append(to_del + space)
@@ -38,6 +67,10 @@ def gen_sent_pair(sent):
             corr.append(word + space)
         else:
             raise SyntaxError(f'{word} is incorrect.')
+
+        if end:
+            orig.append(end)
+            corr.append(end)
 
     return ''.join(orig), ''.join(corr)
 
@@ -93,29 +126,28 @@ def get_spaces_back(words):
 
 
 def prepare_dataset(filename1, filename2, outdir):
-    orig = filename1.read_text(encoding='utf-8-sig')
-    corr = filename2.read_text(encoding='utf-8-sig')
+    orig = filename1.read_text(encoding='utf-8-sig').replace('\n', '').replace('༌', '་')
+    orig = clean_non_bo(orig)
+    orig = re.sub(r'\s+', ' ', orig)
+    corr = filename2.read_text(encoding='utf-8-sig').replace('\n', '').replace('༌', '་')
+    corr = clean_non_bo(corr)
+    corr = re.sub(r'\s+', ' ', corr)
 
-    print('CM diffs')
     sd = SegmentDiff()
     diffs = sd.diff(orig, corr, mode='CM')
     diffs = [a if a else ' ' for a in diffs]  # get back the spaces that are empty strings
     joined = ''.join(diffs)
 
-    print('tokenize')
     tokens = tok.tokenize(joined)
     tokens = join_diffs(tokens)
-    print('sentencify')
     sentences = sentencify(tokens)
     sentences = [[s.content for s in sent] for l, sent in sentences]  # extract strings from sentence tokens
 
-    print('pair sentences')
     # sentence_pairs = [gen_sent_pair(sent) for sent in sentences]
     sentence_pairs = []
     for num, sent in enumerate(sentences):
         sentence_pairs.append(gen_sent_pair(sent))
 
-    print('gen diffs')
     sentence_pairs = [(a.replace(' ', '_'), b.replace(' ', '_')) for a, b in sentence_pairs]
     # ds = SegmentDiff(space_sep_tokens)
     diffs = [sd.diff(t1, t2) for t1, t2 in sentence_pairs]
@@ -126,11 +158,11 @@ def prepare_dataset(filename1, filename2, outdir):
     joined_out = sod.export_diffs(split_context=False)
     sod.write_to_csv(split_out, outdir / (filename1.stem + '_split.csv'))
     sod.write_to_csv(joined_out, outdir / (filename1.stem + '_joined.csv'))
-    print('done')
 
 
 if __name__ == '__main__':
     # make sure the directories exist
+    uptime = time.time()
     orig = Path('input/original')
     corrected = Path('input/corrected')
     outdir = Path('output')
@@ -141,5 +173,10 @@ if __name__ == '__main__':
     for o in orig.glob('*.txt'):
         c = corrected / o.name
         if c.is_file():
-            print(o.name, datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+            print(o.name, end=' ')
+            start = time.time()
             prepare_dataset(o, c, outdir)
+            end = time.time()
+            elapsed = str(datetime.timedelta(seconds=end - uptime))[:7]
+            duration = str(datetime.timedelta(seconds=end - start))[:7]
+            print(f'{duration} ({elapsed} since start)')
